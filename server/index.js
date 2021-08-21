e2bf6f69ab05fb1953444258fe70a9c68657d0f8
FSantiago1987 <rawpower_br@hotmail.com>
2021-08-20 23:05:01 -0700

    deployed project

--- server/index.js
+++ server/index.js
@@ -2,20 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://lonewolf_dev:lonewolf_dev1987@memories.un5e1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello to memories API');
+})
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
